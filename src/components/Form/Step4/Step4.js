e6ea435481ee5edfe1741511c0f55e2c189baf0a
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-03-26 15:54:39 +0000

    Started adding the address fields

--- src/components/Form/Step4/Step4.js
+++ src/components/Form/Step4/Step4.js
@@ -11,10 +11,16 @@ const Step4 = () => {
       </p>
 
       <h3>What company or organisation do you work for?</h3>
-      <Input label="Company or organisation name" name="CompanyName" />
+      <Input name="CompanyName" label="Company or organisation name" />
       <h3>What is your date of birth?</h3>
 
       <h3>What is your home address?</h3>
+      <Input name="AddressLine1" label="Building and street" />
+      <Input name="AddressLine2" />
+
+      <Input name="AddressTown" label="Town or city" />
+      <Input name="AddressLine3" label="County" />
+      <Input name="AddressPostcode" label="Postcode" />
 
       <h3>What is your email address?</h3>
       <h3>What is your telephone number?</h3>
