3315b69f67d7526ec74a6730f913a3c5db6f000a
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-03-26 13:31:54 +0000

    Updated step1 to use grid

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,8 +3,10 @@ import Step1 from 'components/Step1/Step1';
 
 function App() {
   return (
-    <div className="wmnds-container wmnds-p-t-lg wmnds-p-b-lg">
-      <h1>Apply for a ticket refund due to Coronavirus (COVID-19)</h1>
+    <div className="wmnds-container wmnds-p-t-lg wmnds-p-b-lg wmnds-grid">
+      <h1 className="wmnds-col-1 wmnds-col-sm-3-5">
+        Apply for a ticket refund due to Coronavirus (COVID-19)
+      </h1>
       <Step1 />
     </div>
   );
