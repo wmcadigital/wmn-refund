763591d86e7943646122130e5911ebe0f574aaf5
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-03-26 11:59:16 +0000

    Updated app to be more like newer WMN

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Step1 from 'components/Step1/Step1';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="wmnds-container wmnds-p-t-lg wmnds-p-b-lg">
+      <h1>Apply for a ticket refund due to Coronavirus (COVID-19)</h1>
+      <Step1 />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Step1/Step1.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Step1 = () => {
+  return (
+    <div>
+      <p>Use this service to:</p>
+      <ul>
+        <li>
+          Apply for a refund of your ticket if you have been unable to travel due to the Coronavirus
+          outbreak
+        </li>
+      </ul>
+      <p>This process takes around 5 minutes.</p>
+    </div>
+  );
+};
+
+export default Step1;
