82b5f6f0cebc5b8dc17da6dfedea14cec63c6d42
MJP <Martin.Price@wmca.org.uk>
2020-03-26 13:10:10 +0000

    Adds text content to start page
    Adds html content, using design system tags.

--- src/components/Step1/Step1.js
+++ src/components/Step1/Step1.js
@@ -6,12 +6,16 @@ const Step1 = () => {
       <p>Use this service to:</p>
       <ul>
         <li>
-          Apply for a refund of your ticket if you have been unable to travel due to the Coronavirus
-          outbreak
+          Apply for a refund of your Swift card, paper or Swift on Mobile ticket if you have been unable to travel due to Coronavirus
         </li>
       </ul>
       <p>This process takes around 5 minutes.</p>
     </div>
+    <p><strong>Before you start</strong></p>
+    <p>If you have a Swift card, you will need the long number on the front of the card.</p>
+    <p>If the long number begins with <strong>633 597 0112</strong>, it is managed by National Express West Midlands and there is a <a href="https://nxbus.co.uk/west-midlands/news/ticket-refunds-due-to-covid19" title="National Express West Midlands ticket refund application" target="_self" class="wmnds-link">>separate refund application</a>.</p>
+    <p>If you pay by Direct Debit, you will need your direct debit reference number. This begins with <strong>6</strong> and is shown next to every payment to WMCA on your bank statement.</p>
+    <p>If you have a paper ticket, you will need this ticket as proof.</p>
   );
 };
 
