820fc25325ccb94ca7f17f1aa316c35db5a3f686
MJP <Martin.Price@wmca.org.uk>
2020-03-31 11:39:20 +0100

    Change step names (#2)
    Changed step names from nouns to questions
    
    Co-authored-by: Dayle Salmon <13015201+daylesalmon@users.noreply.github.com>

--- src/components/Form/ProgressIndicator/ProgressIndicator.js
+++ src/components/Form/ProgressIndicator/ProgressIndicator.js
@@ -5,10 +5,10 @@ import ProgressIndicatorStep from './ProgressIndicatorStep/ProgressIndicatorStep
 const ProgressIndicator = ({ currentStep }) => {
   return (
     <ul className="wmnds-progress-indicator wmnds-grid wmnds-grid--justify-between">
-      <ProgressIndicatorStep title="Ticket type" stepNumber={1} currentStep={currentStep} />
-      <ProgressIndicatorStep title="Payment" stepNumber={2} currentStep={currentStep} />
-      <ProgressIndicatorStep title="Ticket details" stepNumber={3} currentStep={currentStep} />
-      <ProgressIndicatorStep title="Personal details" stepNumber={4} currentStep={currentStep} />
+      <ProgressIndicatorStep title="What is your ticket type?" stepNumber={1} currentStep={currentStep} />
+      <ProgressIndicatorStep title="How did you but your ticket?" stepNumber={2} currentStep={currentStep} />
+      <ProgressIndicatorStep title="What are your ticket details?" stepNumber={3} currentStep={currentStep} />
+      <ProgressIndicatorStep title="What are your personal details?" stepNumber={4} currentStep={currentStep} />
     </ul>
   );
 };
