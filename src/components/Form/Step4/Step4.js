f414a36658dcef78b4af98889d4402956173285e
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-04-01 09:06:17 +0100

    Created company component

--- src/components/Form/Step4/Step4.js
+++ src/components/Form/Step4/Step4.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Input from 'components/shared/FormElements/Input/Input';
+import Company from './Company/Company';
 
 const Step4 = () => {
   return (
     <>
       <h2>Tell us about yourself</h2>
       <p>
-        We’ll use this information to confirm your identity and contact you if we need more
-        information
+        We’ll use this information to confirm your identity and contact you if
+        we need more information
       </p>
 
-      <h3>What company or organisation do you work for?</h3>
-      <Input name="CompanyName" label="Company or organisation name" />
+      <Company />
+
       <h3>What is your date of birth?</h3>
 
       <h3>What is your home address?</h3>
