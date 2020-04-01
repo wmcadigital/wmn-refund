e84b95611fe8419305c37fde809213567cb07c74
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-04-01 11:55:44 +0100

    Added warning message

--- src/components/Form/Step3/UploadTicket/UploadTicket.js
+++ src/components/Form/Step3/UploadTicket/UploadTicket.js
@@ -33,8 +33,16 @@ const UploadTicket = () => {
 
   return (
     <>
-      <div className="wmnds-fe-group">
-        <h3 className={s.ripTicketTitle}>Rip your paper ticket in half</h3>
+      <div className="wmnds-msg-summary wmnds-msg-summary--warning wmnds-t-b-xl wmnds-m-b-xl">
+        <div className="wmnds-msg-summary__header">
+          <Icon
+            className="wmnds-msg-summary__icon"
+            iconName="general-warning-circle"
+          />
+          <h3 className="wmnds-msg-summary__title">
+            Rip your paper ticket in half
+          </h3>
+        </div>
       </div>
       <div className="wmnds-fe-group">
         <fieldset className="wmnds-fe-fieldset">
