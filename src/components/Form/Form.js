8982ce3fdb134312f40e65e4609894c40d76b880
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-04-01 15:20:15 +0100

    Turned step 4 button into submit

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -11,42 +11,52 @@ import ProgressIndicator from './ProgressIndicator/ProgressIndicator';
 import s from './Form.module.scss';
 
 const Form = () => {
+  const [formState] = useContext(FormContext); // Get the state of form data from FormContext
   const [currentStep, setCurrentStep] = useState(1);
   const [isPaperTicket, setIsPaperTicket] = useState(false); // Used to track if a user is using a paper ticket (set in step 1). Then read this value in step 3 to show 'upload proof/photo'
   const [isSwiftCard, setIsSwiftCard] = useState(false); // Used to track if a user is using a SwiftCard(set in step 1). Then read this value in step 2 to hide 'bought on website radio option'
 
-  const [formState] = useContext(FormContext); // Get the state of form data from FormContext
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  };
 
   return (
     <>
       <div className="wmnds-col-1 wmnds-col-md-3-4 ">
         <ProgressIndicator currentStep={currentStep} />
         <div className={`wmnds-p-lg ${s.formWrapper}`}>
-          {currentStep === 1 && (
-            <Step1
-              setCurrentStep={setCurrentStep}
-              currentStep={currentStep}
-              setIsPaperTicket={setIsPaperTicket}
-              setIsSwiftCard={setIsSwiftCard}
-            />
-          )}
-          {currentStep === 2 && (
-            <Step2
-              setCurrentStep={setCurrentStep}
-              currentStep={currentStep}
-              isSwiftCard={isSwiftCard}
-            />
-          )}
-          {currentStep === 3 && (
-            <Step3
-              setCurrentStep={setCurrentStep}
-              currentStep={currentStep}
-              isPaperTicket={isPaperTicket}
-            />
-          )}
-          {currentStep === 4 && (
-            <Step4 setCurrentStep={setCurrentStep} currentStep={currentStep} />
-          )}
+          {/* Start of form */}
+          <form onSubmit={handleSubmit}>
+            {currentStep === 1 && (
+              <Step1
+                setCurrentStep={setCurrentStep}
+                currentStep={currentStep}
+                setIsPaperTicket={setIsPaperTicket}
+                setIsSwiftCard={setIsSwiftCard}
+              />
+            )}
+            {currentStep === 2 && (
+              <Step2
+                setCurrentStep={setCurrentStep}
+                currentStep={currentStep}
+                isSwiftCard={isSwiftCard}
+              />
+            )}
+            {currentStep === 3 && (
+              <Step3
+                setCurrentStep={setCurrentStep}
+                currentStep={currentStep}
+                isPaperTicket={isPaperTicket}
+              />
+            )}
+            {currentStep === 4 && (
+              <Step4
+                setCurrentStep={setCurrentStep}
+                currentStep={currentStep}
+              />
+            )}
+          </form>
         </div>
       </div>
       <pre
