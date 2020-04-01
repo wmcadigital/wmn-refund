395e5942840e62e4fa3c70edef444e4495d7556c
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-04-01 11:43:15 +0100

    Added red rip ticket

--- src/components/Form/Step3/UploadTicket/UploadTicket.js
+++ src/components/Form/Step3/UploadTicket/UploadTicket.js
@@ -32,40 +32,45 @@ const UploadTicket = () => {
   };
 
   return (
-    <div className="wmnds-fe-group">
-      <fieldset className="wmnds-fe-fieldset">
-        <legend className="wmnds-fe-fieldset__legend">
-          <h3 className="wmnds-fe-question">
-            Upload a photo of the front of the ripped ticket
-          </h3>
-          <p>
-            We need to be able to read the ticket type and expiry date to
-            process the refund
-          </p>
-        </legend>
-        <label
-          htmlFor="fileUpload"
-          className={`wmnds-btn wmnds-btn--primary ${
-            isFileInputFocused ? s.fileUploadLabelFocused : ''
-          }`}
-        >
-          {fileName}
-          <Icon
-            className="wmnds-btn__icon wmnds-btn__icon--right"
-            iconName="general-paperclip"
-          />
-          <input
-            type="file"
-            name="fileUpload"
-            id="fileUpload"
-            onFocus={() => setIsFileInputFocused(true)}
-            onBlur={() => setIsFileInputFocused(false)}
-            onChange={(e) => handleFileSelected(e.target.files[0])}
-            className={s.fileUpload}
-          />
-        </label>
-      </fieldset>
-    </div>
+    <>
+      <div className="wmnds-fe-group">
+        <h3 className={s.ripTicketTitle}>Rip your paper ticket in half</h3>
+      </div>
+      <div className="wmnds-fe-group">
+        <fieldset className="wmnds-fe-fieldset">
+          <legend className="wmnds-fe-fieldset__legend">
+            <h3 className="wmnds-fe-question">
+              Upload a photo of the front of the ripped ticket
+            </h3>
+            <p>
+              We need to be able to read the ticket type and expiry date to
+              process the refund
+            </p>
+          </legend>
+          <label
+            htmlFor="fileUpload"
+            className={`wmnds-btn wmnds-btn--primary ${
+              isFileInputFocused ? s.fileUploadLabelFocused : ''
+            }`}
+          >
+            {fileName}
+            <Icon
+              className="wmnds-btn__icon wmnds-btn__icon--right"
+              iconName="general-paperclip"
+            />
+            <input
+              type="file"
+              name="fileUpload"
+              id="fileUpload"
+              onFocus={() => setIsFileInputFocused(true)}
+              onBlur={() => setIsFileInputFocused(false)}
+              onChange={(e) => handleFileSelected(e.target.files[0])}
+              className={s.fileUpload}
+            />
+          </label>
+        </fieldset>
+      </div>
+    </>
   );
 };
 
