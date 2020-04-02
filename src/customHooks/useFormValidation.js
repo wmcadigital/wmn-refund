68639db78e53680e0a4d56be3f5bf24050f296a6
Dayle Salmon <13015201+daylesalmon@users.noreply.github.com>
2020-04-02 15:53:18 +0100

    Ds validation (#32)
    * Started validation.
    
    * Validation rules almost defined.
    
    * Added error classes to input
    
    * Updated error handling on inputs to match new wmnds styles
    
    * Moved where error is positioned
    
    * Updated validation logic
    
    * Removed onChange/onBlur from input
    
    * Updated erroring
    
    * Form validation now allows custom validation.
    
    * Custom validation can now be passed through

---
+++ src/customHooks/useFormValidation.js
@@ -0,0 +1,55 @@
+import { useState, useContext, useEffect } from 'react';
+// Import contexts
+import { FormContext } from 'globalState/FormContext';
+
+const useInput = (name, label, customValidation) => {
+  // set up the state for the inputs value prop and set it to the default value
+  const [formState, formDispatch] = useContext(FormContext); // Get the state of form data from FormContext
+  // set up state for the inputs error prop
+  const [error, setError] = useState(null);
+  const [isTouched, setIsTouched] = useState(false);
+
+  const value = formState.Application[name] || ''; // Get value from state
+
+  // set up the event handler for onChange event
+  function handleChange(e) {
+    // When input is changed then update state
+    formDispatch({
+      type: 'UPDATE_FORM_DATA',
+      payload: { [name]: e.target.value },
+    });
+  }
+
+  // set up event handler for onBlur
+  function handleBlur() {
+    setIsTouched(true); // Set touched as the input has been touched by user (used below to determine whether to show errors)
+  }
+
+  // Re-use this logic everytime state is updated
+  useEffect(() => {
+    // If the user has touched the input then we can show errors
+    if (isTouched) {
+      // If there is no length
+      if (!value.length) {
+        setError(`${label} is required`);
+      }
+      // Run custom validation logic
+      else if (typeof customValidation === 'function') {
+        customValidation();
+      }
+      // Else all is good, so reset error
+      else {
+        setError(null);
+      }
+    }
+  }, [customValidation, isTouched, label, name, value.length]);
+
+  // return object
+  return {
+    handleChange,
+    handleBlur,
+    error,
+  };
+};
+
+export default useInput;
