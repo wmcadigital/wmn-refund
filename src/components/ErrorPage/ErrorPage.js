54e706029dfc9b9bef1bdb0c09022b8d3c9431ac
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-04-02 10:43:10 +0100

    Split title out and made spacing correct in intro, success, error page(s). Closes #23.

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import Icon from 'components/shared/Icon/Icon';
+import Title from 'components/Title/Title';
 
 const ErrorPage = () => {
   return (
-    <div className="wmnds-col-1 wmnds-col-md-1-2 ">
-      {/* Error message */}
-      <div className="wmnds-msg-summary wmnds-msg-summary--error">
-        <div className="wmnds-msg-summary__header">
-          <Icon
-            iconName="general-warning-triangle"
-            className="wmnds-msg-summary__icon"
-          />
-          <h3 className="wmnds-msg-summary__title">
-            Looks like something went wrong
-          </h3>
-        </div>
+    <>
+      <Title />
+      <div className="wmnds-col-1 wmnds-col-sm-3-4 wmnds-col-lg-1-2">
+        {/* Error message */}
+        <div className="wmnds-msg-summary wmnds-msg-summary--error">
+          <div className="wmnds-msg-summary__header">
+            <Icon
+              iconName="general-warning-triangle"
+              className="wmnds-msg-summary__icon"
+            />
+            <h3 className="wmnds-msg-summary__title">
+              Looks like something went wrong
+            </h3>
+          </div>
 
-        <div className="wmnds-msg-summary__info">
-          Details added what happened and what to do next. Lorem ipsum lorem
-          ipsum. Lorem ipsum lorem ipsum.
+          <div className="wmnds-msg-summary__info">
+            Details added what happened and what to do next. Lorem ipsum lorem
+            ipsum. Lorem ipsum lorem ipsum.
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
