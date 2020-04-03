9d324f7cc3c22fbb59a4f5f77bb6b5301f3946a6
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-04-03 15:47:03 +0100

    Added help text to DOB. Fixes #51.

--- src/components/Form/Step4/DOB/DOB.js
+++ src/components/Form/Step4/DOB/DOB.js
@@ -7,6 +7,7 @@ const DOB = () => {
     <fieldset className="wmnds-fe-fieldset">
       <legend className="wmnds-fe-fieldset__legend">
         <h3 className="wmnds-fe-question">What is your date of birth?</h3>
+        <p>For example, 31 03 1980</p>
       </legend>
       <Date name="DateOfBirth" label="Date of birth" />
     </fieldset>
