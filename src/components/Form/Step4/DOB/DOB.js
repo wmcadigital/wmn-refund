f27c5abf94c876425ab8fde69827baebbd7d2ee0
MJP <Martin.Price@wmca.org.uk>
2020-04-03 15:19:50 +0100

    Adds help text

--- src/components/Form/Step4/DOB/DOB.js
+++ src/components/Form/Step4/DOB/DOB.js
@@ -7,6 +7,7 @@ const DOB = () => {
     <fieldset className="wmnds-fe-fieldset">
       <legend className="wmnds-fe-fieldset__legend">
         <h3 className="wmnds-fe-question">What is your date of birth?</h3>
+        <p>For example, 18 03 2020</p>
       </legend>
       <Date name="DateOfBirth" label="Date of birth" />
     </fieldset>
