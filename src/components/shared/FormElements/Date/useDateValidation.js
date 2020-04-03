ae9b01750136a76195772d035fd1138bba4c0d69
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-04-03 11:35:59 +0100

    Date formating now working

--- src/components/shared/FormElements/Date/useDateValidation.js
+++ src/components/shared/FormElements/Date/useDateValidation.js
@@ -47,9 +47,10 @@ const useInputValidation = (name, label, customValidation) => {
   // Handle validation
   // Re-use this logic everytime state is updated
   useEffect(() => {
-    const dateRegex = /^((((19[0-9][0-9])|(2[0-9][0-9][0-9]))([-])(0[13578]|10|12)([-])(0[1-9]|[12][0-9]|3[01]))|(((19[0-9][0-9])|(2[0-9][0-9][0-9]))([-])(0[469]|11)([-])([0][1-9]|[12][0-9]|30))|(((19[0-9][0-9])|(2[0-9][0-9][0-9]))([-])(02)([-])(0[1-9]|1[0-9]|2[0-8]))|(([02468][048]00)([-])(02)([-])(29))|(([13579][26]00)([-])(02)([-])(29))|(([0-9][0-9][0][48])([-])(02)([-])(29))|(([0-9][0-9][2468][048])([-])(02)([-])(29))|(([0-9][0-9][13579][26])([-])(02)([-])(29)))$/; // Date regex http://regexlib.com/REDetails.aspx?regexp_id=1850
+    setDate(`${year}-${month}-${day}`); // Set date state to current yyyy-mm-dd set by user (would do it in handleChange event but it falls out of sync)
 
-    setDate(`${year}-${month}-${day}`); // Set LastUsed var on how API expects it
+    const dateRegex = /^((((19[0-9][0-9])|(2[0-9][0-9][0-9]))([-])(0[13578]|10|12)([-])(0[1-9]|[12][0-9]|3[01]))|(((19[0-9][0-9])|(2[0-9][0-9][0-9]))([-])(0[469]|11)([-])([0][1-9]|[12][0-9]|30))|(((19[0-9][0-9])|(2[0-9][0-9][0-9]))([-])(02)([-])(0[1-9]|1[0-9]|2[0-8]))|(([02468][048]00)([-])(02)([-])(29))|(([13579][26]00)([-])(02)([-])(29))|(([0-9][0-9][0][48])([-])(02)([-])(29))|(([0-9][0-9][2468][048])([-])(02)([-])(29))|(([0-9][0-9][13579][26])([-])(02)([-])(29)))$/; // Date regex http://regexlib.com/REDetails.aspx?regexp_id=1850
+    const d = new Date().toISOString().slice(0, 10); // Set todays date as yyyy-mm-dd
 
     // If the user has touched the input then we can show errors
     if (isTouched) {
@@ -68,6 +69,10 @@ const useInputValidation = (name, label, customValidation) => {
       // If not a valid date (yyyy-mm-dd)
       else if (!dateRegex.test(date)) {
         setError(`Enter a real ${label.toLowerCase()}`);
+      }
+      // Date must be in future
+      else if (date > d) {
+        setError(`${label} must be today or in the past`);
       }
       // Run custom validation logic
       else if (customValidation) {
