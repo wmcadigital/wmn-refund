cd734e7762f633de54acb2491a572904abc48cdf
MJP <Martin.Price@wmca.org.uk>
2020-04-07 14:54:51 +0100

    Mp paula content changes (#84)
    * Updated wording
    
    * Update claim date
    
    Updated validation, example and error.
    
    * Updates error message further with more help
    
    * Removed SwiftPortal option out of Paper tickets
    
    * Update src/components/Form/Step2/Step2.js
    
    Co-Authored-By: Dayle Salmon <13015201+daylesalmon@users.noreply.github.com>
    
    * Update src/components/Form/Step2/Step2.js
    
    Co-Authored-By: Dayle Salmon <13015201+daylesalmon@users.noreply.github.com>
    
    Co-authored-by: Dayle Salmon <13015201+daylesalmon@users.noreply.github.com>

--- src/components/Form/Step2/Step2.js
+++ src/components/Form/Step2/Step2.js
@@ -37,28 +37,26 @@ const Step2 = ({ currentStep, setCurrentStep, isPaperTicket }) => {
       value: 'DirectDebit',
     },
     {
-      text: 'I bought it from the West Midlands Network or Swift website',
-      value: 'SwiftPortal',
-    },
-    {
       text: 'I pay for it through my company',
       value: 'Corporate',
     },
     {
       text:
-        'I bought it from a ticket office, West Midlands Network travel shop or Payzone shop',
+        'I bought it from a travel shop or Payzone shop',
       value: 'Shop',
     },
   ];
 
   // If the user has selected something other than paper ticket in step 1
   if (!isPaperTicket) {
+    const swiftPortal = { text: 'I bought it from the West Midlands Network or Swift website', value: 'SwiftPortal' };
     const workwise = { text: 'I am on the Workwise scheme', value: 'Workwise' };
     const ticketMachine = {
-      text: 'I bought it from a Swift ticket machine',
+      text: 'I bought it from a Swift kiosk',
       value: 'SwiftPortal',
     };
 
+    radios.splice(-1, 0, swiftPortal); // push SwiftPortal radio option to last before 1 in radio list   
     radios.splice(-1, 0, workwise); // push workwise radio option to last before 1 in radio list
     radios.splice(-1, 0, ticketMachine); // push ticketMachine radio option to last before 1 in radio list
   }
