15ff4e67914bef0ec13926b7388a344380ed5223
MJP <Martin.Price@wmca.org.uk>
2020-04-08 07:40:39 +0100

    Creates Swift on Mobile field
    Too complicated to start editing parts of the paper ticket

---
+++ src/components/Form/Step3/SoMTicketNumber.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// Import components
+import Input from 'components/shared/FormElements/Input/Input';
+
+const TicketNumber = () => {
+  return (
+    <fieldset className="wmnds-fe-fieldset">
+      <legend className="wmnds-fe-fieldset__legend">
+        <h3 className="wmnds-fe-question">What is your ticket number?</h3>
+        <p>
+          This is the number in the history tab of the Swift on Mobile app
+        </p>
+      </legend>
+      <Input
+        className="wmnds-col-1-2 wmnds-col-sm-1-5"
+        name="SoMTicketNumber"
+        label="Ticket number"
+        inputmode="numeric"
+      />
+    </fieldset>
+  );
+};
+
+export default SoMTicketNumber;
