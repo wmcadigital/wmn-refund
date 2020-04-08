f79a194f0466ea7800b1662649f9557b1de64616
Dayle Salmon <13015201+daylesalmon@users.noreply.github.com>
2020-04-08 14:14:17 +0100

    Ds svg inline (#111)
    * Updated the way we inject SVGs to support cross domain/external
    
    * Updated env vars
    
    * Updated logic of svg in index

--- src/components/shared/Icon/Icon.js
+++ src/components/shared/Icon/Icon.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import svgSprite from 'assets/icons/wmnds-svg-sprite.min.svg';
+
+// Due to weird Protocol errors with external SVGs the svg use doesn't work well with production builds
+// So we Ajax the SVG in with a snippet at the bottom of public/index.html
 
 const Icon = ({ className, iconName }) => {
   return (
     <svg className={className}>
-      <use
-        xlinkHref={`${svgSprite}#wmnds-${iconName}`}
-        href={`${svgSprite}#wmnds-${iconName}`}
-      />
+      <use xlinkHref={`#wmnds-${iconName}`} href={`#wmnds-${iconName}`} />
     </svg>
   );
 };
