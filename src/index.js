b25a9d3960a837a224c0c7dbd30abc7110645409
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-04-16 14:57:44 +0100

    Implemented sentry to track errors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+// Helpers
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
+import * as Sentry from '@sentry/browser';
+// React
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+
+// Used to log/track console errors
+Sentry.init({
+  dsn:
+    'https://50233258c9794d6bbe3b694d19405e29@o378798.ingest.sentry.io/5202758',
+});
 
 ReactDOM.render(
   <React.StrictMode>
