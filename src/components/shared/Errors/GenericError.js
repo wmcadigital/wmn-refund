bfe7570291a300dfe27410ef50026993a59334c1
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-04-16 12:35:25 +0100

    Converted SVG to use Icon component

--- src/components/shared/Errors/GenericError.js
+++ src/components/shared/Errors/GenericError.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import Icon from '../Icon/Icon';
 
 const GenericError = () => {
   return (
     <div className="wmnds-msg-summary wmnds-msg-summary--error wmnds-m-b-lg">
       <div className="wmnds-msg-summary__header">
-        <svg className="wmnds-msg-summary__icon">
-          <use
-            xlinkHref="#wmnds-general-warning-triangle"
-            href="#wmnds-general-warning-triangle"
-          />
-        </svg>
+        <Icon
+          iconName="general-warning-triangle"
+          className="wmnds-msg-summary__icon"
+        />
         <h3 className="wmnds-msg-summary__title">There is a problem</h3>
       </div>
 
