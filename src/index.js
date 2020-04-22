92452065b65b1622bfe8742942eeb5fbbed35e02
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-04-22 10:38:54 +0100

    Implemented log rocket privacy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 // Helpers
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
+import LogRocket from 'logrocket';
+import setupLogRocketReact from 'logrocket-react';
 import * as Sentry from '@sentry/browser';
 // React
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+
+// LOG ROCKET
+// Used to log/track console errors with LogRocket
+LogRocket.init('brya8q/wmn-refunds', {
+  // Sanitizer to stop the tracking of user input/text fields
+  dom: {
+    inputSanitizer: true,
+  },
+  // Hide sensitive network requests (calling API with form data)
+  network: {
+    requestSanitizer: (request) => {
+      // if the url contains API URL
+      if (
+        request.url
+          .toLowerCase()
+          .indexOf(process.env.REACT_APP_API_HOST.toLowerCase()) !== -1
+      ) {
+        // Null the body as it/ignore it as it contains user data
+        request.body = null;
+      }
+      // make sure you return the modified request
+      return request;
+    },
+  },
+});
+// after calling LogRocket.init()
+setupLogRocketReact(LogRocket); // Set-up Log Rocket React integration
 
 // Used to log/track console errors
 Sentry.init({
