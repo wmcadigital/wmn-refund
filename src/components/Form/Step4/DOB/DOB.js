86d0f456100b095e8a9ecdd4c06da3e68d963528
Houston <houston@Houstons-MacBook-Air.local>
2020-11-26 10:57:25 +0000

    fix DOB validation mistake

--- src/components/Form/Step4/DOB/DOB.js
+++ src/components/Form/Step4/DOB/DOB.js
@@ -8,17 +8,19 @@ const DOB = () => {
   const { register } = useFormContext(); // Custom hook for handling validation (errors etc)
   const dateValidation = register({
     required: 'Enter a date of birth',
-    pastDate: value => {
-      // make sure date entered is in the past
-      const dateToday = new Date();
-      const dateString = `${dateToday.getFullYear()}-${dateToday.getMonth() + 1}-${dateToday.getDate()}`
-      return dateString > value || 'Date of birth cannot be in the future'
+    validate: {
+      pastDate: value => {
+        // make sure date entered is in the past
+        const dateToday = new Date();
+        const dateString = `${dateToday.getFullYear()}-${dateToday.getMonth() + 1}-${dateToday.getDate()}`
+        return dateString > value || 'Date of birth cannot be in the future'
+      }
     }
   })
   return (
     <fieldset className="wmnds-fe-fieldset">
       <legend className="wmnds-fe-fieldset__legend">
-        <h3 className="wmnds-fe-question">What is your date of birth?</h3>
+        <h2 className="wmnds-fe-question">What is your date of birth?</h2>
         <p>For example, 31 03 1980</p>
       </legend>
       <DateInput
