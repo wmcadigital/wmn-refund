e60018ccdcd915d0de659d88605ff8241fab469a
Restyled.io <commits@restyled.io>
2020-12-03 14:45:53 +0000

    Restyled by prettier

--- src/components/shared/ContactDetails/ContactDetails.js
+++ src/components/shared/ContactDetails/ContactDetails.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function ContactDetails({socialMedia}) {
+function ContactDetails({ socialMedia }) {
   return (
     <div className="wmnds-inset-text">
-        <p>Transport for West Midlands</p>
-        {socialMedia && <p>
-            <a href="https://twitter.com/wmnetwork">Twitter</a><br />
-            <a href="https://www.facebook.com/westmidlandsnetwork">Facebook</a><br />
-        </p>}
-        <p> 
-            Telephone: <a href="tel:03453036760">0345 303 6760<br /></a>
+      <p>Transport for West Midlands</p>
+      {socialMedia && (
+        <p>
+          <a href="https://twitter.com/wmnetwork">Twitter</a>
+          <br />
+          <a href="https://www.facebook.com/westmidlandsnetwork">Facebook</a>
+          <br />
         </p>
-        Monday to Tuesday and Thursday to Friday, 9am - 5pm, <br />
-        Wednesday, 9.30am - 5pm<br />
-        Saturday and Sunday, closed     
+      )}
+      <p>
+        Telephone:{' '}
+        <a href="tel:03453036760">
+          0345 303 6760
+          <br />
+        </a>
+      </p>
+      Monday to Tuesday and Thursday to Friday, 9am - 5pm, <br />
+      Wednesday, 9.30am - 5pm
+      <br />
+      Saturday and Sunday, closed
     </div>
-
   );
 }
 
