d3f66c70ff58484cb7ccc736e5eec187a56b0343
Houston <houston@Houstons-MacBook-Air.local>
2020-12-03 16:02:14 +0000

    update copy (cant process page)

--- src/components/NoProcessPage/NoProcessPage.js
+++ src/components/NoProcessPage/NoProcessPage.js
@@ -8,9 +8,11 @@ const ErrorPage = () => {
       <Title />
       <div className="wmnds-col-1 wmnds-col-md-3-4 wmnds-col-lg-1-2">
         {/* Error message */}
-        <h3>We can't process your refund online yet</h3>
+        <h3>We can&rsquo;t process your refund online yet</h3>
 
-        <p>You'll need to phone Customer Services to get your refund.</p>
+        <p>
+          You&rsquo;ll need to phone Customer Services to request your refund.
+        </p>
 
         <ContactDetails />
       </div>
