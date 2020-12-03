496e8a7f6749456309734c181b538ac73c331199
Houston <houston@Houstons-MacBook-Air.local>
2020-12-03 15:47:47 +0000

    Update get a refund text

--- src/components/shared/Title/Title.js
+++ src/components/shared/Title/Title.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Title = () => {
-  return <h1 className="wmnds-col-1 wmnds-col-lg-3-4">Get a refund</h1>;
+  return (
+    <h1 className="wmnds-col-1 wmnds-col-lg-3-4">Cancel my Direct Debit</h1>
+  );
 };
 
 export default Title;
