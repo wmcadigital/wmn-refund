02a3e76348f9879ee143b9dcf5bbc212bbc7a2dd
GeorgeEverett <George.Everett@wmca.org.uk>
2026-02-17 15:31:36 +0000

    unused import

--- src/components/Form/Step3/CovidRefund/CovidRefund.js
+++ src/components/Form/Step3/CovidRefund/CovidRefund.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import { useEffect, useContext } from 'react';
 import { FormDataContext } from 'globalState/FormDataContext';
 
 const CovidRefund = () => {
