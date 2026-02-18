a3a52a193fb75d7ebad6acd66b74f968fbdf96d8
GeorgeEverett <George.Everett@wmca.org.uk>
2026-02-18 14:28:42 +0000

    import fix

--- src/components/NoProcessPage/NoProcessPage.js
+++ src/components/NoProcessPage/NoProcessPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Title from 'components/shared/Title/Title';
-import ContactDetails from 'components/ContactDetails/ContactDetails';
+import ContactDetails from 'components/shared/ContactDetails/ContactDetails';
 
 const ErrorPage = ({ isFormStarted, setIsFormStarted, setCannotProcess }) => {
   const goBack = () => {
